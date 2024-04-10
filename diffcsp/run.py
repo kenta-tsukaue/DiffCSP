a24496b65adbaf9fe52ffde3d1843cbf1b1f7b1e
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2024-04-10 18:01:02 +0900

    引数をコメントアウト

--- diffcsp/run.py
+++ diffcsp/run.py
@@ -167,7 +167,7 @@ def run(cfg: DictConfig) -> None:
         check_val_every_n_epoch=cfg.logging.val_check_interval,
         #progress_bar_refresh_rate=cfg.logging.progress_bar_refresh_rate,
         enable_progress_bar=True,
-        resume_from_checkpoint=ckpt,
+        #resume_from_checkpoint=ckpt,
         **cfg.train.pl_trainer,
     )
 
