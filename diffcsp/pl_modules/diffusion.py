a397bc01370cc372273f398e2daf0b3cb10613d2
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2024-04-12 15:06:36 +0900

    valを追加

--- diffcsp/pl_modules/diffusion.py
+++ diffcsp/pl_modules/diffusion.py
@@ -275,7 +275,8 @@ class CSPDiffusion(BaseModule):
         output_dict = self(batch)
         log_dict, val_loss = self.compute_stats(output_dict, prefix='val')
         self.log(
-            {'val_loss':val_loss} ,
+            'val_loss',
+            val_loss,
             on_epoch=True, 
             prog_bar=True,
             logger=True) 
