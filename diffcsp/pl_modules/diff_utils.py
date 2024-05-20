13c9100c16fde9a550129c8c082d281b76eac665
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2024-05-20 11:57:08 +0900

    tensorを返すように変更

--- diffcsp/pl_modules/diff_utils.py
+++ diffcsp/pl_modules/diff_utils.py
@@ -92,7 +92,7 @@ def compute_fourier_an(n, sigma, N=10, T=1.0, num_points=1000):
     
     # 係数 a_n の計算
     a_n = 2 * integral  # 0 以外の n に対しては 2 を乗算
-    return a_n.item()  # Python の標準数値型に変換
+    return a_n
 
 def compute_fourier_bn(n, sigma, N=10, T=1.0, num_points=1000):
     # 積分区間 [0, T] を num_points 個の点で離散化
@@ -116,7 +116,7 @@ def compute_fourier_bn(n, sigma, N=10, T=1.0, num_points=1000):
     else:
         b_n = 2 * integral  # 0 以外の n に対しては 2 を乗算
     
-    return b_n.item()  # Python の標準数値型に変換
+    return b_n 
 
 def optimize_mc(x_t, sigma, target1, target2, lr=0.01, iterations=1000):
     
